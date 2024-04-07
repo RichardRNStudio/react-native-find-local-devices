18f1c2ff379c5ad49a3b387e6d77dcf171742913
RichardRNStudio <riiichj@gmail.com>
2024-04-07 21:49:45 +0200

    chore(refactor): refactor the entire package

--- ios/FindLocalDevices.swift
+++ ios/FindLocalDevices.swift
@@ -1,8 +1,8 @@
 @objc(FindLocalDevices)
 class FindLocalDevices: NSObject {
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
 }
---
+++ src/interfaces/IPortScanner.interface.tsx
@@ -0,0 +1,13 @@
+import { IDevice } from './IDevice.interface';
+
+declare type PortScannerCallback = (device: IDevice) => void;
+
+export interface IPortScanner {
+  ports: number[];
+  timeout: number;
+  onDeviceFound: PortScannerCallback;
+  onResults: (device: IDevice[]) => void;
+  onCheck: PortScannerCallback;
+  onFinished: () => void;
+  onError: PortScannerCallback;
+}
